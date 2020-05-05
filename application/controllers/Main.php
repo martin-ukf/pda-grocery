49616a3d28b2a42bee9e3e4e216d215d6daf2557
marti <mm@ukf.sk>
2020-05-05 22:50:42 +0200

    pridana metoda employees v controleri  Main

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,13 +6,30 @@ class Main extends CI_Controller {
 	{
 		parent::__construct();
 
+		/* Standard Libraries of codeigniter are required */
 		$this->load->database();
+		$this->load->helper('url');
+		/* ------------------ */
+
+		$this->load->library('grocery_CRUD');
 
 	}
 
 	public function index()
 	{
 		echo "<h1>Welcome to the world of Codeigniter</h1>";//Just an example to ensure that we get into the function
+		die();
+	}
+
+	public function employees()
+	{
+		$crud = new grocery_CRUD();
+		$crud->set_table('employees');
+		$output = $this->grocery_crud->render();
+
+		echo "<pre>";
+		print_r($output);
+		echo "</pre>";
 		die();
 	}
 }
